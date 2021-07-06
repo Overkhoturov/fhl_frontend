0034b2daf15e794ac20a0e6a06d6d18760ec730f
Oleg <o.verkhoturov@gmail.com>
2021-07-06 20:33:41 +0300

    added redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,9 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   combinedReducers(),
-  compose(applyMiddleware(sagaMiddleware)),
+  compose(applyMiddleware(sagaMiddleware),
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
 );
 
 sagaMiddleware.run(rootSaga);
