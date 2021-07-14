909a29b00ed5bf5505b8bc047df3ff4cded29c76
Oleg <o.verkhoturov@gmail.com>
2021-07-14 20:03:58 +0300

    delete dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -8,9 +8,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   combinedReducers(),
-  compose(applyMiddleware(sagaMiddleware),
-    // eslint-disable-next-line no-underscore-dangle
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+  compose(applyMiddleware(sagaMiddleware)),
 );
 
 sagaMiddleware.run(rootSaga);
