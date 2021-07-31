9c1bf4b7ad2cf467359861c9d55bca3f8d922f30
Mot <matvey2000sergeev@mail.ru>
2021-07-31 12:49:52 +0300

    first_try

--- src/pages/Donuts/index.jsx
+++ src/pages/Donuts/index.jsx
@@ -4,11 +4,54 @@ import Header from '../../components/Header';
 
 import './index.css';
 
-export default (() => (
-  <>
-    <Header />
-    <div>
-      Donuts
-    </div>
-  </>
-));
+export default (() => {
+  const danatPeople = [
+    { name: 'Алексей', foto: 'user.svg' },
+    { name: 'Лиза', foto: 'user.svg' },
+    { name: 'Степан', foto: 'user.svg' },
+    { name: 'Николай', foto: 'user.svg' },
+    { name: 'Николай', foto: 'user.svg' },
+    { name: 'Эмилия', foto: 'user.svg' },
+    { name: 'Алексей', foto: 'user.svg' },
+    { name: 'Стас', foto: 'user.svg' },
+    { name: 'Алексей', foto: 'user.svg' },
+    { name: 'Евгения', foto: 'user.svg' },
+  ];
+
+  return (
+    <>
+      <Header />
+      <div classNameName="danate-service">
+        <div className="container">
+          <h1 className="title-line">
+            ПОДДЕРЖАТЬ ПРОЕКТ
+          </h1>
+          <p className="title-line__description">
+            Мы проводим турниры, трансляции, тренировки,
+            и просто встречи единомышленников по League of Legends.
+            Если вам нравится наша деятельность, вы можете поддержать нас через VK Donut.
+          </p>
+          <p className="danate-service__btn">
+            Поддержать
+          </p>
+        </div>
+      </div>
+
+      <div className="container">
+        <h2 className="title-line">
+          ДОНЫ НАШЕГО СООБЩЕСТВА
+        </h2>
+        <div className="danate-cards">
+          {danatPeople.foreach((el) => {
+            <div className="danate-card">
+              <img className="danate-card__image" alt="" />
+              <p className="danate-card__title">
+                {el.name}
+              </p>
+            </div>;
+          })}
+        </div>
+      </div>
+    </>
+  );
+});
