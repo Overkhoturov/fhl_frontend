8a9c39ad1034915398044345f84c5513c8495167
Sanya <sanyan1of@gmail.com>
2021-07-31 17:47:14 +0300

    update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,33 +8,31 @@ import './index.css';
 
 const Footer = () => (
   <footer className="footer">
-    <div className="container">
-      <ul className="footer-menu">
-        <li className="footer-menu__item">
-          <Link to="/" className="footer-menu__link">Турниры</Link>
-        </li>
-        <li className="footer-menu__item">
-          <Link to="players" className="footer-menu__link">Игроки</Link>
-        </li>
-        <li className="footer-menu__item">
-          <Link to="toclubs" className="footer-menu__link">Клубы</Link>
-        </li>
-        <li className="footer-menu__item">
-          <Link to="todanate" className="footer-menu__link">Поддержать</Link>
-        </li>
-        <li className="footer-menu__item--social">
-          <a className="footer-menu__social" href="https://vk.com/firehorn">
-            <VkIcon />
-          </a>
-          <a className="footer-menu__social" href="https://www.youtube.com/channel/UChaA9krZaM5qgR6-u0YE2pAe">
-            <YoutubeIcon />
-          </a>
-          <a className="footer-menu__social" href="https://www.instagram.com/firehornleague/">
-            <InstagramIcon />
-          </a>
-        </li>
-      </ul>
-    </div>
+    <ul className="footer-menu">
+      <li className="footer-menu__item">
+        <Link to="/" className="footer-menu__link">Турниры</Link>
+      </li>
+      <li className="footer-menu__item">
+        <Link to="players" className="footer-menu__link">Игроки</Link>
+      </li>
+      <li className="footer-menu__item">
+        <Link to="toclubs" className="footer-menu__link">Клубы</Link>
+      </li>
+      <li className="footer-menu__item">
+        <Link to="todanate" className="footer-menu__link">Поддержать</Link>
+      </li>
+      <li className="footer-menu__item--social footer-menu__item">
+        <a className="footer-menu__social" href="https://vk.com/firehorn">
+          <VkIcon />
+        </a>
+        <a className="footer-menu__social" href="https://www.youtube.com/channel/UChaA9krZaM5qgR6-u0YE2pAe">
+          <YoutubeIcon />
+        </a>
+        <a className="footer-menu__social" href="https://www.instagram.com/firehornleague/">
+          <InstagramIcon />
+        </a>
+      </li>
+    </ul>
   </footer>
 );
 export default memo(Footer);
