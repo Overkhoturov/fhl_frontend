3ad5092b57a157c23594770ec35f290938a928ee
Oleg <o.verkhoturov@gmail.com>
2021-08-04 00:29:39 +0300

    adde rank and show current data

--- src/constants.js
+++ src/constants.js
@@ -21,6 +21,25 @@ export default {
     SUPPORT: 'Саппорт',
   },
 
+  RANKS: {
+    IRON: ['IRON', 'Железо'],
+    BRONZE: ['BRONZE', 'Бронза'],
+    SILVER: ['SILVER', 'Серебро'],
+    GOLD: ['GOLD', 'Золото'],
+    PLATINUM: ['PLATINUM', 'Платина'],
+    DIAMOND: ['DIAMOND', 'Алмаз'],
+    MASTER: ['MASTER', 'Мастер'],
+    GRANDMASTER: ['GRANDMASTER', 'Грандмастер'],
+    CHALLENGER: ['CHALLENGER', 'Претендент'],
+  },
+
+  DIVISIONS: {
+    4: [4, 'IV'],
+    3: [3, 'III'],
+    2: [2, 'II'],
+    1: [1, 'I'],
+  },
+
   TOURNAMENTS_TABS: [
     { text: 'сетка', name: 'challonge_link' },
     { text: 'составы', name: 'teams' },
