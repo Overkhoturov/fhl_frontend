48bb9560b68c503a33d7b67f3d3bf1d54ae544c4
Sanya <sanyan1of@gmail.com>
2021-08-08 21:14:21 +0300

    remove tournaments rules preoader

--- src/pages/Tournaments/TabRegular.jsx
+++ src/pages/Tournaments/TabRegular.jsx
@@ -3,25 +3,17 @@ import PropTypes from 'prop-types';
 
 const TabRegular = memo((props) => {
   const { regulation } = props;
-  const showLogo = !regulation;
   return (
     <>
-      <h2 className="title-line">
-        Регламент
-      </h2>
-      {showLogo ? (
-        <div className="net-loader">
-          <div className="spinner-border" role="status" />
-          <span>
-            Регламент отсутствует
-          </span>
-        </div>
-      )
-        : (
-          <article className="article">
-            {regulation}
-          </article>
-        )}
+      <div className="container">
+        <h2 className="title-line">
+          Регламент
+        </h2>
+        <article className="article">
+          {regulation}
+          <p>Текст регламента</p>
+        </article>
+      </div>
     </>
   );
 });
