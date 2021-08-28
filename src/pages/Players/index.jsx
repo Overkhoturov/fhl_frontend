01657cb482245c16dcb2bae5ad86f7b06aa376a2
Sanya <sanyan1of@gmail.com>
2021-08-28 09:17:53 +0300

    upd players ranks

--- src/pages/Players/index.jsx
+++ src/pages/Players/index.jsx
@@ -4,9 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-import './index.css';
-import Player from './player';
+import './index.scss';
 import { getPlayers } from '../../actions/players';
+import Player from './player';
 
 const Players = memo(() => {
   const dispatch = useDispatch();
@@ -22,26 +22,36 @@ const Players = memo(() => {
   if (error) {
     return <div>Ошибочка</div>;
   }
+  const wholePlayers = players.map((p) => (
+    <Player
+      id={p.id}
+      nickname={p.nick_name}
+      rank={p.rank}
+      main_role={p.main_role}
+      additional_roles={p.additional_roles}
+    />
+  ));
+
   return (
     <>
       <div className="main">
         <Header isShowNavigation />
         <div className="container">
-          <h2 className="title-line">Игроки</h2>
-          <div className="table">
-            <div className="table__row">
-              <div className="table__col table__col--head table__col--1 table__col--num">№</div>
-              <div className="table__col table__col--head table__col--3">Ник</div>
-              <div className="table__col table__col--head table__col--2">Ранг</div>
-              <div className="table__col table__col--head table__col--2">Позиция</div>
-              <div className="table__col table__col--head table__col--2">Доп</div>
+          <h2 className="title-line">
+            Игроки
+          </h2>
+          <div className="players-table-header">
+            <div className="players-table-subheader">
+              <div className="players-table-subheader__item-first">№</div>
+              <div className="players-table-subheader__item-first">Ник</div>
             </div>
-            <div className="table__rows">
-              <Player />
-              <Player />
-              <Player />
+            <div className="players-table-subheader">
+              <div className="players-table-subheader__item-second">Ранг</div>
+              <div className="players-table-subheader__item-second">Позиция</div>
+              <div className="players-table-subheader__item-second">Доп</div>
             </div>
           </div>
+          {wholePlayers}
         </div>
       </div>
       <Footer />
--- src/pages/Players/player.jsx
+++ src/pages/Players/player.jsx
@@ -1,44 +1,108 @@
-import React from 'react';
-import './index.css';
+import React, { memo } from 'react';
+import './index.scss';
+import PropTypes from 'prop-types';
 
-const Player = () => (
-  <div>
-    <div className="table__col table__col--1 table__col--num">1</div>
-    <div className="table__col table__col--3">d Album</div>
-    <div className="table__col table__col--2">
-      <img
-        className="player__ranked"
-        src="img/ranked-emblems/Emblem_Gold.png"
-        title="Золото II"
-        alt="Золото II"
-      />
-    </div>
-    <div className="table__col table__col--2">
-      <img
-        className="player__position"
-        src="img/ranked-positions/Position_Gold-Jungle.png"
-        title="Лес"
-        alt="Лес"
-      />
-    </div>
-    <div className="table__col table__col--2">
-      <div className="player__positions-list">
-        <img
-          className="player__position"
-          src="img/ranked-positions/Position_Gold-Top.png"
-          title="Топ"
-          alt="Топ"
-        />
-        <img
-          className="player__position"
-          src="img/ranked-positions/Position_Gold-Mid.png"
-          title="Мид"
-          alt="Мид"
-        />
+import bronzeRank from '../../assets/img/ranked-emblems/Emblem_Bronze.png';
+import silverRank from '../../assets/img/ranked-emblems/Emblem_Silver.png';
+import ironRank from '../../assets/img/ranked-emblems/Emblem_Iron.png';
+import goldRank from '../../assets/img/ranked-emblems/Emblem_Gold.png';
+import platinumRank from '../../assets/img/ranked-emblems/Emblem_Platinum.png';
+import masterRank from '../../assets/img/ranked-emblems/Emblem_Master.png';
+import diamondRank from '../../assets/img/ranked-emblems/Emblem_Diamond.png';
+import challengerRank from '../../assets/img/ranked-emblems/Emblem_Challenger.png';
+import grandmasterRank from '../../assets/img/ranked-emblems/Emblem_Grandmaster.png';
+
+const Player = memo((props) => {
+  let rankSrc;
+  switch (props.rank) {
+    case 'IRON 1':
+    case 'IRON 2':
+    case 'IRON 3':
+      rankSrc = ironRank;
+      break;
+    case 'BRONZE 1':
+    case 'BRONZE 2':
+    case 'BRONZE 3':
+      rankSrc = bronzeRank;
+      break;
+    case 'SILVER 1':
+    case 'SILVER 2':
+    case 'SILVER 3':
+      rankSrc = silverRank;
+      break;
+    case 'GOLD 1':
+    case 'GOLD 2':
+    case 'GOLD 3':
+      rankSrc = goldRank;
+      break;
+    case 'PLATINUM 1':
+    case 'PLATINUM 2':
+    case 'PLATINUM 3':
+      rankSrc = platinumRank;
+      break;
+    case 'DIAMOND 1':
+    case 'DIAMOND 2':
+    case 'DIAMOND 3':
+      rankSrc = diamondRank;
+      break;
+    case 'MASTER 1':
+    case 'MASTER 2':
+    case 'MASTER 3':
+      rankSrc = masterRank;
+      break;
+    case 'GRANDMASTER 1':
+    case 'GRANDMASTER 2':
+    case 'GRANDMASTER 3':
+      rankSrc = grandmasterRank;
+      break;
+    case 'CHALLENGER 1':
+    case 'CHALLENGER 2':
+    case 'CHALLENGER 3':
+      rankSrc = challengerRank;
+      break;
+
+    default:
+      rankSrc = null;
+  }
+
+  const mainPosSrc = 'https://steamcdn-a.akamaihd.net/apps/csgo/blog/images/service_medal_2021.png';
+  const secondPosImg = <img className="players-pos" src="https://steamcdn-a.akamaihd.net/apps/csgo/blog/images/service_medal_2021.png" alt="" />;
+  return (
+    <div className="players-table-header">
+      <div className="players-table-subheader">
+        <div className="players-table-subheader__item-first">{props.id}</div>
+        <div className="players-table-subheader__item-first">{props.nickname}</div>
+      </div>
+      <div className="players-table-subheader">
+
+        <div className="players-table-subheader__item-second">
+          <img
+            className="players-rang"
+            src={rankSrc}
+            alt=""
+          />
+        </div>
+        <div className="players-table-subheader__item-second"><img className="players-pos" src={mainPosSrc} alt="" /></div>
+        <div className="players-table-subheader__item-second">{secondPosImg}</div>
       </div>
     </div>
-  </div>
+  );
+});
 
-);
+Player.propTypes = {
+  id: PropTypes.number,
+  nickname: PropTypes.string,
+  rank: PropTypes.string,
+  // main_role: PropTypes.string,
+  // additional_roles: PropTypes.string,
+};
+
+Player.defaultProps = {
+  id: null,
+  nickname: null,
+  rank: PropTypes.string,
+  // main_role: PropTypes.string,
+  // additional_roles: PropTypes.string,
+};
 
 export default Player;
